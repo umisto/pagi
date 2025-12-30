8f4fafb9786dc695315e4046f2e359ee0f9f6152
trpdjke <yehorushakov@gmail.com>
2025-12-30 22:58:37 +0200

    go back to page

--- http.go
+++ http.go
@@ -1,62 +1,55 @@
 package pagi
 
-//func GetLimit(r *http.Request) int {
-//	limit := 20
-//
-//	if s := r.URL.Query().Get("limit"); s != "" {
-//		n, err := strconv.Atoi(s)
-//		if err == nil {
-//			limit = n
-//		}
-//	}
-//
-//	if limit <= 0 {
-//		limit = 20
-//	}
-//	if limit > 100 {
-//		limit = 100
-//	}
-//
-//	return limit
-//}
-//
-//func GetSort(r *http.Request) *SortField {
-//	sortStr := strings.TrimSpace(r.URL.Query().Get("sort"))
-//	if sortStr == "" {
-//		return nil
-//	}
-//
-//	sort := &SortField{
-//		Ascend: true,
-//	}
-//
-//	if strings.HasPrefix(sortStr, "-") {
-//		sort.Ascend = false
-//		sort.Field = strings.TrimPrefix(sortStr, "-")
-//	} else {
-//		sort.Field = sortStr
-//	}
-//
-//	sort.Field = strings.TrimSpace(sort.Field)
-//	if sort.Field == "" {
-//		return nil
-//	}
-//
-//	return sort
-//}
-//
-//func GetCursor(r *http.Request) *Cursor {
-//	s := strings.TrimSpace(r.URL.Query().Get("cursor"))
-//	if s == "" {
-//		return nil
-//	}
-//	return &Cursor{Value: s}
-//}
-//
-//func GetParams(r *http.Request) Params {
-//	return Params{
-//		Cursor: GetCursor(r),
-//		Sort:   GetSort(r),
-//		Limit:  GetLimit(r),
-//	}
-//}
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func GetLimit(
+	r *http.Request,
+	max uint,
+) uint {
+	limit := uint(20)
+
+	if s := r.URL.Query().Get("limit"); s != "" {
+		parsed, err := strconv.ParseUint(s, 10, 64)
+		if err == nil {
+			limit = uint(parsed)
+		}
+	}
+
+	if limit == 0 {
+		limit = 20
+	}
+	if limit > max {
+		limit = max
+	}
+
+	return limit
+}
+
+func GetSort(r *http.Request) *SortField {
+	sortStr := strings.TrimSpace(r.URL.Query().Get("sort"))
+	if sortStr == "" {
+		return nil
+	}
+
+	sort := &SortField{
+		Ascend: true,
+	}
+
+	if strings.HasPrefix(sortStr, "-") {
+		sort.Ascend = false
+		sort.Field = strings.TrimPrefix(sortStr, "-")
+	} else {
+		sort.Field = sortStr
+	}
+
+	sort.Field = strings.TrimSpace(sort.Field)
+	if sort.Field == "" {
+		return nil
+	}
+
+	return sort
+}
